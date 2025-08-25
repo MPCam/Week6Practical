65f75224c6915abed7c9543dec61f6a11fb28da5
Morgan Campbell <20241069@mywhitecliffe.com>
2025-08-25 14:05:37 +1200

    Updates test and workflow

--- test_1.py
+++ test_1.py
@@ -6,7 +6,8 @@ from Example1 import safe_divide
     "a,b,expected", [
         (6, 2, 3.0),
         (5, 0, float('inf')),
-        (-10, -2, 5.0)
+        (-10, -2, 5.0),
+        (20, 2, 8)
     ]
 )
 def test_safe_divide(a, b, expected):
